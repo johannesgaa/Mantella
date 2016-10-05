fa2698ca9bdea94ee1476bdfd2153b3d19034b82
OpusV <pusz@sra.uni-hannover.de>
2016-10-05 16:58:04 +0200

    Added lambert function

--- myWorkspace/main.cpp
+++ myWorkspace/main.cpp
@@ -2,13 +2,26 @@
 
 int main(){
   
+  std::cout << " --- stumpffC --- " << std::endl;
   std::cout << "stumpffC(100) " << mant::itd::stumpffC(100) << std::endl;
   std::cout << "stumpffC(-100) " << mant::itd::stumpffC(-100) << std::endl;
   std::cout << "stumpffC(0) " << mant::itd::stumpffC(0) << std::endl << std::endl;
-  
+
+  std::cout << " --- stumpffS --- " << std::endl;  
   std::cout << "stumpffS(100) " << mant::itd::stumpffS(100) << std::endl;
   std::cout << "stumpffS(-100) " << mant::itd::stumpffS(-100) << std::endl;
-  std::cout << "stumpffS(0) " << mant::itd::stumpffS(0) << std::endl;
+  std::cout << "stumpffS(0) " << mant::itd::stumpffS(0) << std::endl << std::endl;
+  
+  std::cout << " --- Lambert --- " << std::endl;
+  std::pair<arma::Col<double>, arma::Col<double>> velocities = mant::itd::lambert(
+    {5000.0, 10000.0, 2100.0}, 
+    {-14600.0, 2500.0, 7000.0}, 
+    3600.0, 
+    true); 
+  std::cout << "v1 " << std::endl << velocities.first << std::endl;
+  std::cout << "v2 " << std::endl << velocities.second << std::endl << std::endl;
+  
+  std::cout << " --- Planet --- " << std::endl;
   
   return 0;
 }
--- myWorkspace/orbitalMechanics.hpp
+++ myWorkspace/orbitalMechanics.hpp
@@ -3,17 +3,16 @@
 // C++ standard library
 #include <cmath>
 #include <iostream>
+#include <functional>
 
 // Armadillo
 #include <armadillo>
 
 // Mantella
-#include "../include/mantella" //apt-get install liblapacke-dev
+#include <mantella>
 
 namespace mant{
   namespace itd{
-    
-    const double heliocentricGravitationalConstant = 1.32712440018e11; //km^3 / s^2
     
     double stumpffC(
         const double parameter){
@@ -51,6 +50,55 @@ namespace mant{
       }
     }
     
+    std::pair<arma::Col<double>::fixed<3>, arma::Col<double>::fixed<3>> lambert(
+        const arma::Col<double>::fixed<3> &startPosition,
+        const arma::Col<double>::fixed<3> &endPosition,
+        const double flightTime,
+        const bool isPrograde){
+      
+      double startPositionNorm = arma::norm(startPosition);
+      double endPositionNorm = arma::norm(endPosition);
+      
+      arma::Col<double> positionsCrossProduct = arma::cross(startPosition, endPosition);
+      double theta = std::acos(arma::norm_dot(startPosition, endPosition));
+      
+      if(isPrograde){
+        if(positionsCrossProduct(2) < 0){
+          theta = 2.0 * arma::datum::pi - theta;
+        }
+      }else{
+        if(positionsCrossProduct(2) >= 0){
+          theta = 2.0 * arma::datum::pi - theta;
+        }
+      }
+
+      double a = std::sin(theta) * std::sqrt((startPositionNorm * endPositionNorm) / (1.0 - std::cos(theta)));
+      
+      std::function<double(const double)> yFunction = [&](
+          const double parameter){ 
+        return startPositionNorm + endPositionNorm + a * ((parameter * stumpffS(parameter) - 1.0) / std::sqrt(stumpffC(parameter)));
+      };
+      
+      double z = mant::brent(
+        [&](
+            const double parameter) { 
+
+          return stumpffS(parameter) * std::pow(yFunction(parameter) / stumpffC(parameter), 3.0/2.0) + a * std::sqrt(yFunction(parameter)) - std::sqrt(3.986e+5) * flightTime; //heliocentric should be 1.32712440018e11
+          
+        }, -2.0, 2.0, 1000); //TODO: remove magic numbers
+      
+      double y = yFunction(z);
+
+      double f = 1.0 - y / startPositionNorm;
+      double g = a * std::sqrt(y / 3.986e+5); //heliocentric should be 1.32712440018e11
+      double gDot = 1.0 - y / endPositionNorm;
+      
+      return {
+        {(1.0 / g) * (endPosition - f * startPosition)}, 
+        {(1.0 / g) * (gDot * endPosition - startPosition)}
+      };
+      
+    }
     
   }
 }
